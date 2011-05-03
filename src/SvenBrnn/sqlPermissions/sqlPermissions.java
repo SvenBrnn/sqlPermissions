2d1faeeb20ab5aaf71cb45f2e020acf42eda432c
SvenBrnn <svenbrnn@web.de>
2011-05-03 12:25:27 +0200

    Init Clear Projekt

---
+++ src/SvenBrnn/sqlPermissions/sqlPermissions.java
@@ -0,0 +1,48 @@
+package SvenBrnn.sqlPermissions;
+
+import java.util.HashMap;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+/**
+ * sqlPermissions for Bukkit
+ *
+ * @author SvenBrnn
+ */
+public class sqlPermissions extends JavaPlugin {
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>(); 
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here including the registration of any events
+
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+       
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+    public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        System.out.println("Goodbye world!");
+    }
+    public boolean isDebugging(final Player player) {
+        if (debugees.containsKey(player)) {
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) {
+        debugees.put(player, value);
+    }
+}
+
