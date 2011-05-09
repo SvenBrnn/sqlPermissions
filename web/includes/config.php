d90b344a58f2a3b75b935ad2d201a2c18498c5e0
SvenBrnn <svenbrnn@web.de>
2011-05-09 10:48:53 +0200

    Config Change

---
+++ web/includes/config.php
@@ -0,0 +1,8 @@
+<?php
+define('TEMPLATE', 'default'); //Active Template
+
+define('SQL_USER', 'permissions'); //SQL User
+define('SQL_PASS', 'password'); //SQL Password
+define('SQL_DATABASE', 'permissions'); //SQL Database
+define('SQL_HOST', 'localhost'); //SQL Host
+?>
